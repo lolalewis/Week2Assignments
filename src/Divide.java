d189ea7bcfbdd60f1bf377a51d1233892f2888f2
lola1590 <lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-18 11:57:45 -0400

    school work

--- src/Divide.java
+++ src/Divide.java
@@ -4,6 +4,14 @@ public class Divide {
  
     public static void main(String[] args) {
       Scanner s = new Scanner(System.in);
+      int num1,num2;
+      int remain ;
+        System.out.print("Enter an Interger ");
+        num1 =s.nextInt();
+         System.out.print("Enter an Interger ");
+        num2=s.nextInt();
+        
+        System.out.format("%d" + (num1%num2));
     }
     
 }
