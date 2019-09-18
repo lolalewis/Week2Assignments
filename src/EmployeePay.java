4afb72c37ea43a9a0c5370435c509925f38fdc56
lola1590 <lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-18 11:34:35 -0400

    school

--- src/EmployeePay.java
+++ src/EmployeePay.java
@@ -4,6 +4,20 @@ public class EmployeePay {
  
     public static void main(String[] args) {
       Scanner s = new Scanner(System.in);
+      final double  WAGE=15,overtime=22.50;
+      double totalpay,pay=0, hours=0;
+      ;
+      //5 employees
+        for (int i = 0; i <= 5; i++) {
+             System.out.print("Enter hours worked for Employee " + (i+1) );
+             hours = s.nextDouble();
+             
+        }while(hours>40){
+        pay = hours*overtime;     
+       
+      
+      
     }
     
+}
 }
