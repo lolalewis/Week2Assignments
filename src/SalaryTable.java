55298875d105086038bad16785cd45b0402b1a31
lola1590 <lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-19 11:52:10 -0400

    updated from school

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -7,21 +7,25 @@ public class SalaryTable {
         int increase;       
         int finish;
         int raise=0;
+        
         //Headings
         System.out.format("%20s%20s%20s%20s\n","YEAR", "OLD SALARY", "RAISE", "NEW SALARY" );
         System.out.format("%20s%20s%20s%20s\n","----", "---------", "-----", "----------" );
         
         for (int year = 1; year < 10; year++) {
-            System.out.format("%d%20d",year, start);
-            increase = (int)(start * 0.03);
+            System.out.format("%d%40d\n",year, start);
+           
+            increase = (int )(start * 0.03);
             start+=increase;
             raise= start-increase;
-            finish= start+increase;
-           
-            System.out.format("%10d%10d%10d%10d",year,start,increase,finish);
-        }
+            finish= start+increase;          
+            System.out.format("%20d%20d%20d%20d\n",year,start,increase,finish);
+            
+       
+        
         
         
     }
     
+}
 }
