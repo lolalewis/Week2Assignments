1172e60b3a7d524df44575f92f8d7b8d54d380f1
lola1590 <lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-19 11:52:03 -0400

    updated from school

--- src/PowerChart.java
+++ src/PowerChart.java
@@ -4,6 +4,26 @@ public class PowerChart {
   
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int num1=0;
+       
+        System.out.print("Enter a number 0->10: ");
+        num1 = s.nextInt();
+        //format
+        System.out.format("%20s%20s\n", "X", "2^X");
+        System.out.format("%21s%19s\n","--", "---");
+        
+        for (int i = 0; i <= 10; i++) {
+            
+            System.out.println(Math.pow(2,num1));
+          
+            System.out.println("");
+                      
+        }
+        
+        
+        
+        
+            
     }
     
 }
