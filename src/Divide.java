f336330ad9b11e6c5a979da84dafd77758bd4131
paolakaren <paolakaren@192.168.0.13>
2019-09-24 01:34:23 -0400

    work from home

--- src/Divide.java
+++ src/Divide.java
@@ -6,12 +6,17 @@ public class Divide {
       Scanner s = new Scanner(System.in);
       int num1,num2;
       int remain ;
+      int quotient;
         System.out.print("Enter an Interger ");
         num1 =s.nextInt();
          System.out.print("Enter an Interger ");
         num2=s.nextInt();
+       
+       quotient=num1>num2? num1/num2 : num2/num1;
+       remain=num1>num2? num1%num2 : num2%num1;
+              
+        System.out.format("The answer is %d Remainder %d" ,quotient,remain );
         
-        System.out.format("%d" + (num1%num2));
     }
     
 }
